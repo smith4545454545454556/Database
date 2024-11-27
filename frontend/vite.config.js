98e50ddd0d8ea424e8fc36c455c5350a20232dd2
smith4545454545454556 <smith.maharjan1233@gmail.com>
2024-11-27 18:57:26 +0545

    database

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://work-mu-sandy.vercel.app"
+      "/api": "https://database-beta-ten.vercel.app"
     },
   },
   plugins: [react()],
